0bdbb20ce3b5e1b7ca9e6a7aea3b91c477da421a
Ardy Chen <ardy.studio@gmail.com>
2017-11-17 17:01:14 +0800

    add command into page object.

--- test/e2e/specs/todo.page.object.spec.js
+++ test/e2e/specs/todo.page.object.spec.js
@@ -9,22 +9,6 @@ let todoInputList = [
   '789'
 ];
 
-const addTodoByList = (todoToolsSection, todoInputList) => {
-  todoInputList.map((todoInput) => {
-    todoToolsSection
-      .clearValue('@todoInput')
-      .setValue('@todoInput', todoInput)
-      .click('@addButton')
-  });
-}
-
-const validateTodoList = (browser, todoInputList) => {
-  todoInputList.map((todoInput, index) => {
-    let i = index + 1;
-    browser.assert.containsText('#todoList>li:nth-child('+ i +')', todoInput);
-  });
-}
-
 module.exports = {
   'Test Todo by Page Object': function (browser) {
     var todoPage = browser.page.Todo();
@@ -35,21 +19,20 @@ module.exports = {
       .waitForElementPresent('body', 2000)
       .assert.title('Nightwatch Demo')
 
-    // todoPage.navigate();
     todoPage.expect.section('@todoTools').to.be.visible;
 
     todoToolsSection.expect.element('@todoInput').to.be.visible;
     todoToolsSection.expect.element('@addButton').to.be.visible;
     todoToolsSection.expect.element('@reverseButton').to.be.visible;
 
-    addTodoByList(todoToolsSection, todoInputList);
-    validateTodoList(browser, todoInputList);
+    todoPage.addTodoByList(todoInputList);
+    todoPage.validateTodoList(todoInputList);
 
     todoToolsSection.click('@reverseButton');
 
     browser.pause(1000);
 
-    validateTodoList(browser, todoInputList.reverse());
+    todoPage.validateTodoList(todoInputList.reverse());
 
     browser.end();
   },
--- test/pages/Todo.js
+++ test/pages/Todo.js
@@ -1,8 +1,29 @@
 /**
  * Created by Ardy Chen on 11/14/2017.
  */
+var commands = {
+  addTodoByList: function(todoInputList) {
+    var todoPage = this.api.page.Todo();
+    var todoToolsSection = todoPage.section.todoTools;
+    todoInputList.map((todoInput) => {
+      return todoToolsSection.clearValue('@todoInput')
+        .setValue('@todoInput', todoInput)
+        .click('@addButton')
+    });
+  },
+  validateTodoList: function(todoInputList) {
+    var browser = this.api;
+
+    todoInputList.map((todoInput, index) => {
+      let i = index + 1;
+      browser.assert.containsText('#todoList>li:nth-child('+ i +')', todoInput);
+    });
+  }
+}
+
 module.exports = {
   url: 'http://localhost:3000/',
+  commands: [commands],
   sections: {
     todoTools: {
       selector: '#todoCreator',
